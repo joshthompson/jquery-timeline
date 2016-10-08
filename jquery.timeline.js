2d6bc581bdb3c921c95a02eed38f9c036ba596c2
Josh Thompson <josh3276@gmail.com>
2016-10-08 11:36:08 +0200

    Cleaned up code a little bit

--- jquery.timeline.js
+++ jquery.timeline.js
@@ -12,29 +12,27 @@
 (function($) {
 	$.fn.timeline = function(items, options) {
 
-		// Setup vars
+		// Setup jQuery elements
 		var $timeline = $(this).addClass("jqtimeline").empty();
-
-		// Setup container sections
 		var $left_col = $("<div class='jqtimeline-column jqtimeline-column-left'></div>");
 		var $line = $("<div class='jqtimeline-line'></div>");
 		var $right_col = $("<div class='jqtimeline-column jqtimeline-column-right'></div>");
 		$timeline.append($left_col).append($line).append($right_col);
 
-		var label = false;
 
-		// Add items
+		// Loop through items
+		var label = false;
 		$.each(items, function(i, $item) {
 
-			$main_col  = $right_col.height() >= $left_col.height() ? $left_col : $right_col;
-			$other_col = $right_col.height() >= $left_col.height() ? $right_col : $left_col;
+			// Check which column is taller and choose that column to add to
+			$col  = $right_col.height() >= $left_col.height() ? $left_col : $right_col;
 
-			$empty_box = i ? $("<div class='jqtimeline-empty'></div>") : "";
-
+			// Create elements to be added to the column and add them
+			var $empty_box = i ? $("<div class='jqtimeline-empty'></div>") : "";
 			var $item_box = $("<div class='jqtimeline-item' data-jqtimeline-item='" + i + "'></div>").append($item);
+			$col.append($empty_box).append($item_box);
 
-			$main_col.append($empty_box).append($item_box);
-
+			// Check if label is different from label before... if it is, we use it... normally used for date
 			if ($item.attr("data-jqtimeline-label") && label != $item.attr("data-jqtimeline-label")) {
 				var $label = $("<div class='jqtimeline-label' data-jqtimeline-item='" + i + "'>" + $item.attr("data-jqtimeline-label") + "</div>");
 				$line.append($label);
@@ -44,11 +42,11 @@
 		});
 
 
-		// Do something else
+		// Set all the values that depends on the window and container size so when they resize properly
 		var resizeWindowFunction = function() {
 			// Setup column widths
 			var innerWidth = $timeline.width();
-			var lineWidth = 24;
+			var lineWidth = $line.outerWidth() + parseInt($line.css("margin-left")) + parseInt($line.css("margin-right"));
 			var columnWidth = Math.floor(innerWidth / 2 - lineWidth / 2);
 			$timeline.find(".jqtimeline-column").width(columnWidth);
 
@@ -67,6 +65,8 @@
 			});
 
 		};
+		
+		// Run resize function for initial setup and bind it to a window resize event
 		resizeWindowFunction();
 		$(window).resize(resizeWindowFunction);
 
